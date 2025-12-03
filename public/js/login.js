87787a7dee9bec2adcfefc5a177eec15bc45fd6a
Dilan_Gael <hdz.g4el@gmail.com>
2025-12-02 21:49:19 -0600

    feat: actualizar URLs del frontend

--- public/js/login.js
+++ public/js/login.js
@@ -3,6 +3,7 @@ const registerForm = document.getElementById("registerForm");
 const toggleLink = document.getElementById("toggleLink");
 const toggleMessage = document.getElementById("toggleMessage");
 const formTitle = document.getElementById("formTitle");
+const API_BASE_URL = 'https://bibliotech-backend-s219.onrender.com';
 
 
 
@@ -46,7 +47,7 @@ document.getElementById('registerForm').addEventListener('submit', async (e) => 
     numero: e.target.number.value,
   }
   try {
-    const res = await fetch(`/api/usuarios`, {
+    const res = await fetch(`${API_BASE_URL}/api/usuarios`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -74,7 +75,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     password: e.target.contra.value
   }
   try {
-    const response = await fetch(`/api/usuarios/login`, {
+    const response = await fetch(`${API_BASE_URL}/api/usuarios/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
